fdbb3c96a52a7624fc97b7b7dadb1e4ae53b907b
samra26 <75056386+samra26@users.noreply.github.com>
2023-01-24 02:10:58 +0500

    Update RGBDincomplete.py

--- RGBDincomplete.py
+++ RGBDincomplete.py
@@ -31,12 +31,12 @@ class RGBDInModule(nn.Module):
         
 
     def forward(self, x):
-
+        B,C,H,W=x.shape
         x,x1= self.backbone(x)
         '''for i in range(len(x1)):
             print('The backbone features are',x1[i].shape)
         print('finish looping')'''
-        return x1
+        return x1,B,H,W
 
 
 class RGBD_incomplete(nn.Module):
@@ -44,11 +44,18 @@ class RGBD_incomplete(nn.Module):
         super(RGBD_incomplete, self).__init__()
         
         self.RGBDInModule = RGBDInModule
+        self.conv1x1=nn.Conv2d(144, 1,1, 1)
 
         
     def forward(self, f_all):
-        x1 = self.RGBDInModule(f_all)
-        return x1[1]
+        x1,B,H,W = self.RGBDInModule(f_all)
+        x_out=x1[1]
+        _,_,C=x_out.shape
+        x_out=x_out.transpose(-2,-1).contiguous().view(B, C, H/4, W/4)
+        print('x_out',x_out.shape)
+        x_out=self.conv1x1(x_out)
+        print('x_outa',x_out.shape)
+        return x_out
 
 
 def build_model(network='cswin', base_model_cfg='cswin'):
