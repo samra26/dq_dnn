2ddbdf199e50e3f766b6a78a2dd8cad4140e74f9
samra26 <75056386+samra26@users.noreply.github.com>
2023-01-24 02:34:22 +0500

    Update losses.py

--- losses.py
+++ losses.py
@@ -5,9 +5,9 @@ def hausdorff(x, y):
     y=y.squeeze()
     x = x.float()
     y = y.float()
-    
+    print(x.shape,y.shape)
     distance_tensor = torch.cdist(x, y, p=2)
-    
+    print(distance_tensor.shape)
     
     value1 = distance_tensor.min(2)[0].max(1, keepdim=True)[0]
     value2 = distance_tensor.min(1)[1].max(1, keepdim=True)[0]
